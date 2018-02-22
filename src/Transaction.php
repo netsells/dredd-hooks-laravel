d326a2ca9a7d6ec9683cd2d479d3697daab3782f
Sam Jordan <sam@netsells.co.uk>
2018-02-22 08:40:46 +0000

    Added more transaction helpers

--- src/Transaction.php
+++ src/Transaction.php
@@ -11,10 +11,23 @@ class Transaction
         $this->transaction = $transaction;
     }
 
+    public function removeQueryStringFromUrl()
+    {
+        $pathParts = explode('?', $this->transaction->fullPath);
+        $this->transaction->fullPath = $pathParts[0];
+    }
+
     public function replaceInPath($old, $new)
     {
         $this->transaction->request->uri = str_replace($old, $new, $this->transaction->request->uri);
         $this->transaction->fullPath = str_replace($old, $new, $this->transaction->fullPath);
+    }
+
+    public function alterBody(\Closure $closure)
+    {
+        $requestBody = json_decode($this->transaction->request->body);
+        $requestBody = $closure($requestBody);
+        $this->transaction->request->body = json_encode($requestBody);
     }
 
     /**
