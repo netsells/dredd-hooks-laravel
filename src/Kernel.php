a5743b48a8404ae26c1fcff0585f915790ee1f08
Adam <adam@bandhosting.nl>
2019-01-07 16:30:14 +0100

    Added more Dredd hooks to code

--- src/Kernel.php
+++ src/Kernel.php
@@ -17,10 +17,20 @@ abstract class Kernel
 
     public function __call($method, $args)
     {
-        if (in_array($method, ['beforeAll', 'beforeEach'])) {
+        if (in_array($method, ['beforeAll', 'beforeEach', 'beforeEachValidation', 'afterEach', 'afterAll'])) {
             $callable = $this->resolveArgument($args[0]);
 
             Hooks::$method(function (&$transaction) use ($callable) {
+                $transactionObject = new Transaction($transaction);
+                $callable($transactionObject);
+                $transaction = $transactionObject->getTransaction();
+            });
+        }
+
+        if (in_array($method, ['before', 'beforeValidation', 'after'])) {
+            $callable = $this->resolveArgument($args[1]);
+
+            Hooks::$method($args[0], function (&$transaction) use ($callable) {
                 $transactionObject = new Transaction($transaction);
                 $callable($transactionObject);
                 $transaction = $transactionObject->getTransaction();
