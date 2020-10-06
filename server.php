aa70ef90cb9d1c9880e7ea4c818af890dd921cf3
adam <adam@nostradamus.nu>
2020-10-06 08:52:28 +0200

    compatiblity with new DotEnv setup

--- server.php
+++ server.php
@@ -7,8 +7,8 @@ require $basePath . '/vendor/autoload.php';
 
 // Laravel will not overwrite existing environment variables, so we'll pull in custom ones here
 if (file_exists($basePath . '/.env.dredd')) {
-    $dotenv = new Dotenv\Dotenv(new \Dotenv\Loader([$basePath . '/.env.dredd'], new \Dotenv\Environment\DotenvFactory()));
-    $dotenv->overload();
+    $dotenv = Dotenv\Dotenv::createMutable($basePath, '.env.dredd');
+    $dotenv->load();
 }
 
 require_once $basePath . '/server.php';
